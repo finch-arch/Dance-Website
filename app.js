e68311731f463bb0e46c9748178995a8a367a505
finch <vishalmahto003@gmail.com>
2023-03-27 19:45:19 +0530

    Added Contact page

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.set('views' , path.join(__dirname , 'views')) //set the views directory
 //END points
 app.get('/' , (req, res)=>{
     const param ={}
-    res.status(200).render('index.pug' , param);
+    res.status(200).render('home.pug' , param);
 })
+app.get('/contact' , (req, res)=>{
+    const param ={}
+    res.status(200).render('contact.pug' , param);
+})
+
 
 //start the server
 
