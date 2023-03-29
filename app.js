26494c2e7e2f7fec6d2e07464738b69d40a5eae6
finch <vishalmahto003@gmail.com>
2023-03-29 19:53:13 +0530

    not working contact work

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const path = require("path"); 
 const app = express();
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser");
+
+mongoose.connect('mongodb://localhost:80/contactDance', {useNewUrlParser: true});
 const port = 80;
 
-//Express specific stuff
-app.use('/static' , express.static('static'))
+
+// Define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+var Contact = mongoose.model('Contact', contactSchema);
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
 app.use(express.urlencoded())
 
-//Pug specific stuff
-app.set('view engine' , 'pug') //set the template engine as pug
-app.set('views' , path.join(__dirname , 'views')) //set the views directory
-
-//END points
-app.get('/' , (req, res)=>{
-    const param ={}
-    res.status(200).render('home.pug' , param);
-})
-app.get('/contact' , (req, res)=>{
-    const param ={}
-    res.status(200).render('contact.pug' , param);
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('home.pug', params);
 })
 
+app.get('/contact', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('contact.pug', params);
+})
 
-//start the server
 
-app.listen(port , ()=>{
-    console.log(`The application has started successfully on port ${port}`)
+app.post('/contact', (req, res)=>{ 
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved to the database")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    });
+
+    // res.status(200).render('contact.pug');
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
 });
